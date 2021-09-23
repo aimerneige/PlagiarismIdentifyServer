4944ef988b58ff5ff5b26827c594fa1e7b9f63ba
Aimer Neige <aimer.neige.soft@gmail.com>
2021-09-23 22:03:30 +0800

    update hid generate

---
+++ utils/hid_generate.go
@@ -0,0 +1,21 @@
+package utils
+
+import (
+	"crypto/rand"
+	"io"
+)
+
+var table = [...]byte{'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'}
+
+// GenerateHid Generate Homework Id
+func GenerateHid(max int) string {
+	b := make([]byte, max)
+	n, err := io.ReadAtLeast(rand.Reader, b, max)
+	if n != max {
+		panic(err)
+	}
+	for i := 0; i < len(b); i++ {
+		b[i] = table[int(b[i])%len(table)]
+	}
+	return string(b)
+}
