f3728b1f64ebe7a88b8f631d92edb8d7f3b00e20
Aimer Neige <aimer.neige.soft@gmail.com>
2021-10-02 20:54:10 +0800

    remove

--- route/all_route.go
+++ route/all_route.go
@@ -5,7 +5,6 @@ import "github.com/gin-gonic/gin"
 func AllRouteCollection(r *gin.Engine) *gin.Engine {
 	r = IndexRouteCollection(r)
 	r = UserRouteCollection(r)
-	r = ArticleRouteCollection(r)
 
 	return r
 }
