daf8caf45e4025ee1adcad1bbc1b11ad6371ac60
Aimer Neige <aimer.neige.soft@gmail.com>
2021-10-05 21:10:29 +0800

    update

--- utils/verify_util.go
+++ utils/verify_util.go
@@ -1,21 +1,32 @@
+// Copyright (c) 2021 AimerNeige
+// aimer.neige@aimerneige.com
+// All rights reserved.
+
 package utils
 
 import (
 	"regexp"
 )
 
-// Verify the legality of the data
+const (
+	EMIAL_VERIFY_PATTERN = `\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*`
+	PHONE_VERIFY_PATTERN = `^(?:\+?86)?1(?:3\d{3}|5[^4\D]\d{2}|8\d{3}|7(?:[0-35-9]\d{2}|4(?:0\d|1[0-2]|9\d))|9[0-35-9]\d{2}|6[2567]\d{2}|4(?:(?:10|4[01])\d{3}|[68]\d{4}|[579]\d{2}))\d{6}$`
+)
 
 // VerifyEmailFormat verify email format
 func VerifyEmailFormat(email string) bool {
-	pattern := `\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*`
-	reg := regexp.MustCompile(pattern)
+	if email == "" {
+		return false
+	}
+	reg := regexp.MustCompile(EMIAL_VERIFY_PATTERN)
 	return reg.MatchString(email)
 }
 
 // VerifyChinaPhoneNumberFormat verify china phone number format
 func VerifyChinaPhoneNumberFormat(phoneNumber string) bool {
-	pattern := `^(?:\+?86)?1(?:3\d{3}|5[^4\D]\d{2}|8\d{3}|7(?:[0-35-9]\d{2}|4(?:0\d|1[0-2]|9\d))|9[0-35-9]\d{2}|6[2567]\d{2}|4(?:(?:10|4[01])\d{3}|[68]\d{4}|[579]\d{2}))\d{6}$`
-	reg := regexp.MustCompile(pattern)
+	if phoneNumber == "" {
+		return false
+	}
+	reg := regexp.MustCompile(PHONE_VERIFY_PATTERN)
 	return reg.MatchString(phoneNumber)
 }
