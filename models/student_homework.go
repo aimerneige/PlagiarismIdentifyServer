c76ccdc2ba1619388b5a9a47016e684d01bb6e40
Aimer Neige <aimer.neige.soft@gmail.com>
2021-10-06 12:13:45 +0800

    update isPlagiarism

--- models/student_homework.go
+++ models/student_homework.go
@@ -12,20 +12,22 @@ import (
 
 type StudentHomework struct {
 	gorm.Model
-	StudentID   uint
-	HomeworkID  uint
-	FileItemIDs []uint
-	Upload      bool
-	UploadTime  time.Time
+	StudentID    uint
+	HomeworkID   uint
+	FileItemIDs  []uint
+	Upload       bool
+	UploadTime   time.Time
+	IsPlagiarism bool
 }
 
 type StudentHomeworkDto struct {
-	ID          uint      `json:"id"`
-	StudentID   uint      `json:"studentId"`
-	HomeworkID  uint      `json:"homeworkId"`
-	FileItemIDs []uint    `json:"fileItemIds"`
-	Upload      bool      `json:"upload"`
-	UploadTime  time.Time `json:"uploadTime"`
+	ID           uint      `json:"id"`
+	StudentID    uint      `json:"studentId"`
+	HomeworkID   uint      `json:"homeworkId"`
+	FileItemIDs  []uint    `json:"fileItemIds"`
+	Upload       bool      `json:"upload"`
+	UploadTime   time.Time `json:"uploadTime"`
+	IsPlagiarism bool      `json:"isPlagiarism"`
 }
 
 func (s StudentHomework) ToDto() (dto StudentHomeworkDto) {
@@ -35,6 +37,7 @@ func (s StudentHomework) ToDto() (dto StudentHomeworkDto) {
 	dto.FileItemIDs = s.FileItemIDs // TODO test for bug
 	dto.Upload = s.Upload
 	dto.UploadTime = s.UploadTime
+	dto.IsPlagiarism = s.IsPlagiarism
 
 	return
 }
