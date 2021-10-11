323e1a6f91cb29b25f4f4c50dde0c4a2b6a4a943
Aimer Neige <aimer.neige.soft@gmail.com>
2021-10-11 17:31:37 +0800

    update generate shell

--- raml/generate_html.sh
+++ raml/generate_html.sh
@@ -1,5 +1,41 @@
+#!/bin/bash
 # Copyright (c) 2021 AimerNeige
 # aimer.neige@aimerneige.com
 # All rights reserved.
 
-raml2html api.raml > api.html
+PS3='Please enter your choice: '
+options=("raml2html-default-theme" "raml2html-plain-theme" "raml2html-slate-theme" "raml2html-kaa-theme" "raml2html-full-markdown-theme" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "raml2html-default-theme")
+            echo "you chose raml2html-default-theme"
+            raml2html --theme 'raml2html-default-theme' api.raml > api.html
+            break
+            ;;
+        "raml2html-plain-theme")
+            echo "you chose raml2html-plain-theme"
+            raml2html --theme 'raml2html-plain-theme' api.raml > api.html
+            break
+            ;;
+        "raml2html-slate-theme")
+            echo "you chose raml2html-slate-theme"
+            raml2html --theme 'raml2html-slate-theme' api.raml > api.html
+            break
+            ;;
+        "raml2html-kaa-theme")
+            echo "you chose raml2html-kaa-theme"
+            raml2html --theme 'raml2html-kaa-theme' api.raml > api.html
+            break
+            ;;
+        "raml2html-full-markdown-theme")
+            echo "you chose raml2html-kaa-theme"
+            raml2html --theme 'raml2html-full-markdown-theme' api.raml > api.md
+            break
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
