f02ca4c33156030b4274f594fc46d295e593ac8c
Aimer Neige <aimer.neige.soft@gmail.com>
2021-10-16 22:26:17 +0800

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,10 @@
 # All rights reserved.
 
 ./release_build.sh
+cd ./raml
+raml2html --theme 'raml2html-default-theme' api.raml > api.html
+raml2html --theme 'raml2html-full-markdown-theme' api.raml > api.md
+cd ..
 scp ./release/PlagiarismIdentidyServer root@39.105.116.248:/root/plagiarism-identify-server/PlagiarismIdentidyServer
 scp ./raml/api.html root@39.105.116.248:/root/plagiarism-identify-server/file/doc/api.html
 scp ./raml/api.md root@39.105.116.248:/root/plagiarism-identify-server/file/doc/api.md
